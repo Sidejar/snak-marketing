dd9a174fa2f83ba7475f0e3c1951efae5e278664
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-27 17:15:49 +0500

    added typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "#131418",
+      primary1: "#23262F",
+      grayColor: "#565A6C",
+      white: "#fff",
+      black: "#000",
+      inputColor: "#6C6D75",
+      textColor: "#717171",
+
+      bgPrimary: "#F0F4FF",
+      bgWhite: "#fff",
+    },
     extend: {},
   },
   plugins: [],

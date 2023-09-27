f90c45578b3613d31e30a0a8e0675cf8cf6c5a58
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-27 17:54:28 +0500

    add typography

--- src/components/countries.js
+++ src/components/countries.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const Countries = ({ flag, countryName }) => {
   return (
-    <div className="w-[160px] bg-[##F0F4FF] flex flex-col justify-center gap-4 py-3">
+    <div className="w-[160px] bg-bgPrimary flex flex-col items-center gap-4 py-3 rounded-[20px]">
       <img src={flag} alt="flag_icon" className="w-12" />
-      <p>{countryName}</p>
+      <p className="p-large !font-bold">{countryName}</p>
     </div>
   )
 }
---
+++ src/components/voice.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Voice = () => {
+  return (
+    <div className="w-[527px] pr-10 pb-[76px]">
+      <div>
+        <span>Vær hilset! Hvordan har du det?</span>
+      </div>
+    </div>
+  )
+}
+
+export default Voice
